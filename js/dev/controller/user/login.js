42ebc391e79bb81f6028a12636724089af00614d
programlife <lizjut@gmail.com>
2014-12-09 11:18:48 +0800

    login return status

--- js/dev/controller/user/login.js
+++ js/dev/controller/user/login.js
@@ -1,4 +1,4 @@
-define(["cryptojs-sha256"], function(crypto) {
+﻿define(["cryptojs-sha256"], function(crypto) {
 	return ["loginCtrl", ["$scope", "$location", "$http", "$cookies", "$window", function($scope, $location, $http, $cookies, $window) {
 		if($scope.isLogin){
 			$location.path("todoList");
@@ -46,7 +46,7 @@ define(["cryptojs-sha256"], function(crypto) {
 			$http.post("/userBiz/login", $scope.UserInfo).
 			// $http.post("/test/todo/login.php", $scope.UserInfo).
 				success(function(data){
-					if(data.loginStatus == '0'){
+					if(data == 'success'){
 						$cookies.isLogin = true;
 						$cookies.role = data.role;
 						alert('用户登录成功');
